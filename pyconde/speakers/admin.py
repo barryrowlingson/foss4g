efd4d2986b3e0ef98a295f42a07e5cc4bc08587b
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2013-06-17 15:35:29 +0100

    added affiliation to speaker admin

--- pyconde/speakers/admin.py
+++ pyconde/speakers/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Speaker,
-    list_display=("user",))
+class SpeakerAdmin(admin.ModelAdmin):
+    models = models.Speaker
+    fields = ("user","affiliation")
+    list_display=("user","affiliation")
+    list_editable=("user","affiliation")
+
+admin.site.register(models.Speaker,SpeakerAdmin)
+

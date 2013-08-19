585334c02e705258436fcf19120422133b43c20b
Barry Rowlingson <b.rowlingson@gmail.com>
2013-08-19 22:20:45 +0100

    added plenaries to schedule output

--- pyconde/programme/giggity.py
+++ pyconde/programme/giggity.py
@@ -7,6 +7,8 @@ from django.template.defaultfilters import slugify
 from django.template.loader import render_to_string
 from django.conf import settings
 
+import codecs
+
 PRES_ID=10000
 PLENARY_ID=20000
 EVENT_ID=30000
@@ -46,8 +48,25 @@ def make_xml():
 
         Ss = sorted(S,key=lambda x: (x.day,x.location_id))
 
+        plenaries = models.PlenarySession.objects.all().select_related(depth=2)
+        for plenary in plenaries:
+            plenary.day = (plenary.start.date() - settings.FIRST_DAY).days + 1
+            duration = (plenary.end() - plenary.start).seconds/60
+            hrs = duration / 60
+            mins = duration % 60
+            plenary.duration = "%02d:%02d" % (hrs,mins)
+            plenary.items = plenary.plenaryitem_set.all().order_by("position")
+            plenary.pid = 5000+plenary.pk
+            
         context = {"Phash": Phash,
-                   "S": Ss}
+                   "S": Ss,
+                   "plenaries":plenaries}
         return render_to_string("giggity/schedule.xml",
                                 context,
                                 )
+
+def writeXML(filepath):
+    f=codecs.open(filepath,encoding="utf-8",mode="w")
+    xml = make_xml()
+    f.write(xml)
+    f.close()
